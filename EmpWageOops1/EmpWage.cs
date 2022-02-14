1db1532917c9a00b19110c26d707f53b82a851c4
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-14 17:07:02 +0530

    Daliy Wage is added

--- EmpWageOops1/EmpWage.cs
+++ EmpWageOops1/EmpWage.cs
@@ -8,5 +8,35 @@ namespace EmpWageOops1
 {
     internal class EmpWage
     {
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 0;
+        const int IS_PRESENT = 1;
+        const int IS_ABSENT = 0;
+        const int RATE_PER_HOUR = 20;
+
+       
+        Random random = new Random();
+        public void GetAttendance()
+        {
+          
+            int checkAttendance = random.Next(0, 2);
+            if (checkAttendance == IS_PRESENT)
+                Console.WriteLine("Is Present");
+            else if (checkAttendance == IS_ABSENT)
+                Console.WriteLine("Is Absent");
+        }
+        public void GetDailyWage()
+        {
+            int dailyWage = 0;
+            int dailyHours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                dailyHours = 8;
+            else if (empCheck == IS_PART_TIME)
+                dailyHours = 4;
+            dailyWage = dailyHours * RATE_PER_HOUR;
+            Console.WriteLine("Daily Wage: " + dailyWage);
+        }
     }
 }
